70959deae67aae01d89af4b6fad5d9fbdaf3fb92
InaDoWo <ina.wortmann@gmail.com>
2017-06-09 16:26:30 +0200

    Update country-detail.component.js
    API problem fixed

--- app/country-detail/country-detail.component.js
+++ app/country-detail/country-detail.component.js
@@ -8,12 +8,9 @@ component('countryDetail', {
             $http.get('https://restcountries.eu/rest/v1/alpha/' + $routeParams.countryAlpha2Code).then(function(response) {
                 self.country = response.data;
             });
-            $http.get('http://api.openweathermap.org/data/2.5/forecast/city?q=' + $routeParams.capital + '&lang=de&units=metric&APPID=a39a0395d8d318e22241cf4a05cf50e2').then(function(response) {
+            $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + $routeParams.capital + '&lang=de&units=metric&APPID=a39a0395d8d318e22241cf4a05cf50e2').then(function(response) {
                 self.weather = response.data;
             });
-            // $http.get('http://api.sunrise-sunset.org/json?lat=' + $routeParams.lat + '&lng=' + $routeParams.lng).then(function(response) {
-            //     self.sun = response.data;
-            // });
 
             self.master = {};
 
